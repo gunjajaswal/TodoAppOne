5ac9cf0748989cce58ef5c1afbcd72bb7a35e398
Gunja Jaiswal <gunjajaiswal738@gmail.com>
2023-01-27 02:38:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todoappone/ui/TaskFragment.kt
@@ -0,0 +1,65 @@
+package com.example.todoappone.ui
+
+import android.app.AlertDialog
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import com.example.todoappone.R
+import com.example.todoappone.TodoApplication
+import com.example.todoappone.databinding.FragmentTaskBinding
+
+class TaskFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TaskFragment()
+    }
+    private lateinit var binding: FragmentTaskBinding
+    private val viewModel: TaskViewModel by activityViewModels{
+        InventoryViewModelFactory((activity?.application as TodoApplication).database.taskDao())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentTaskBinding.inflate(inflater,container,false)
+        binding.viewModel=viewModel
+        binding.lifecycleOwner=this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fabAdd.setOnClickListener{
+            showAddTaskDialog(it)
+        }
+    }
+
+    private fun showAddTaskDialog(it: View?) {
+        val dialog=AlertDialog.Builder(requireContext())
+            .setTitle("Add Task")
+            .setView(R.layout.add_task_dailog_layout)
+            .setPositiveButton("Add"){dailog,_ ->
+                val taskname=
+                    (dailog as AlertDialog).findViewById<EditText>(R.id.editTitle)?.text.toString()
+                if (viewModel.addItem(title = taskname)){
+                    Toast.makeText(requireContext(),"Task Added",Toast.LENGTH_SHORT).show()
+
+                }
+            }
+            .setPositiveButton("Cancel"){dialog, _ ->
+                dialog.dismiss()
+            }
+            .setCancelable(false)
+        dialog.show()
+
+    }
+
+
+}
